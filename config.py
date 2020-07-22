621a110ea2b0023c05289439260d98de892c44b2
Pedro69491 <48108176+Pedro69491@users.noreply.github.com>
2020-07-22 14:40:48 +0100

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from os import getenv # get environment variables
-from dotenv import load_dotenv  # load configuration from .env 
-
-load_dotenv() # it opens the .env
-
-class Config:
-    SECRET_KEY = getenv('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
